36f8685247424bb100047c44a4437b5009bca1d9
Jason McCormack <jason.mccormack@druva.com>
2018-12-29 13:16:29 -0800

    Added sockets capability

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const path = require('path');
+const http = require('http');
+
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -8,9 +11,19 @@ console.log(__dirname + '/../public');
 console.log(publicPath);
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+io.on('connection', (socket) => {
+    console.log('New user Connected');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    })
+});
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Running on port ${port}`);
 });
